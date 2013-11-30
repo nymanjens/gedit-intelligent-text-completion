0b876e0f4f184ae9127fbaf3c21773607601ae59
Jens Nyman <nymanjens.nj@gmail.com>
2013-11-30 15:22:01 +0000

    fixed bug in Options for gedit 3

--- gedit3/intelligent_text_completion.py
+++ gedit3/intelligent_text_completion.py
@@ -10,21 +10,21 @@
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 
-from gi.repository import Gtk, GObject, Gedit
+from gi.repository import Gtk, GObject, Gedit, PeasGtk
 import re
 import traceback
 import gconf
 
-class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
+class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable, PeasGtk.Configurable):
     window = GObject.property(type=Gedit.Window)
 
     def __init__(self):
         GObject.Object.__init__(self)
         self._instances = {}
-    
-    def create_configure_dialog(self):
-        return options_singleton().create_configure_dialog()
-    
+
+    def do_create_configure_widget(self):
+        return IntelligentTextCompletionOptions.get_instance().create_configure_dialog()
+
     def _connect_view(self, view, window):
         """Connect to view's editing signals."""
         callback = self._on_view_key_press_event
@@ -72,7 +72,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
             err = "Exception\n"
             err += traceback.format_exc()
             doc.set_text(err)
-    
+
     ############ plugin core functions ############
     def _handle_event(self, view, event, window):
         """Key press event"""
@@ -113,13 +113,13 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
             whitespace += preceding_line[whitespace_pos]
             whitespace_pos += 1
         # get options
-        options = options_singleton()
-        
+        options = IntelligentTextCompletionOptions.get_instance()
+
         # Do not complete text after pasting text.
         if len(typed_string) > 1:
             return False
         typed_char = typed_string
-        
+
         # GLOBALS
         open_close = {
             '"': '"',
@@ -128,7 +128,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
             '{': '}',
             '[': ']',
         }
-        
+
         ################### selected text ###################
         bounds = doc.get_selection_bounds()
         if len(bounds) > 0:
@@ -150,13 +150,13 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                         doc.insert_at_cursor(close)
                         return True
             return False
-        
+
         ################### auto-close brackets and quotes ###################
         if options.closeBracketsAndQuotes and prev_char != '\\':
             """ detect python comments """
             if typed_char == '"' and re.search('^[^"]*""$', preceding_line) and cursor.ends_line():
                 return self._insert_at_cursor(typed_char + ' ', ' """')
-            
+
             for check_char, add_char in open_close.items():
                 # if character user is adding is the same as the one that
                 # is auto-generated, remove the auto generated char
@@ -207,7 +207,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                 if event.keyval == 65288: # backspace
                     if prev_char == check_char and next_char == add_char:
                         doc.delete(cursor, next_char_pos)
-        
+
         ################### auto-complete XML tags ###################
         if options.completeXML:
             if prev_char == "<" and typed_char == "/":
@@ -220,7 +220,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                     return self._insert_at_cursor(typed_char + closing_tag + ">")
                 else:
                     return False # do nothing
-        
+
         ################### detect lists ###################
         if options.detectLists:
             if event.keyval == 65293: # return
@@ -237,7 +237,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                                 doc.delete(start, cursor)
                                 return True
                             return self._insert_at_cursor(typed_char + whitespace + bullet)
-        
+
         ################### detect java-like comment ###################
         if event.keyval == 65293: # return
             # constants
@@ -251,7 +251,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                     add_middle = typed_char + whitespace + comment_middle
                     add_end = typed_char + whitespace + comment_end
                     return self._insert_at_cursor(add_middle, add_end)
-        
+
         ################### auto-indent after function/list ###################
         if options.autoindentAfterFunctionOrList:
             if event.keyval == 65293: # return
@@ -264,7 +264,7 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                 for indent_trigger, ending_char in indent_triggers.items():
                     if prev_char == indent_trigger:
                         if line_after:
-                            # text between begin and ending brackets should come 
+                            # text between begin and ending brackets should come
                             # in the middle row
                             if ending_char != '' and ending_char in line_after:
                                 ending_pos = line_after.find(ending_char)
@@ -274,15 +274,15 @@ class IntelligentTextCompletionPlugin(GObject.Object, Gedit.WindowActivatable):
                             end.set_line_offset(end.get_line_offset() + ending_pos)
                             ending_text = doc.get_text(cursor, end, False).strip()
                             doc.delete(cursor, end)
-                            
+
                             add_middle = typed_char + whitespace + get_tab_string(view)
                             add_end = ending_text + typed_char + whitespace
                         else:
                             add_middle = typed_char + whitespace + get_tab_string(view)
                             add_end = ""
                         return self._insert_at_cursor(add_middle, add_end)
-                    
-        
+
+
     def _insert_at_cursor(self, middle, end = ""):
         window = self.window
         doc = window.get_active_document()
@@ -342,135 +342,103 @@ def get_closing_xml_tag(document):
 
 
 ################## OPTIONS DIALOG ##################
-def options_singleton():
-    if Options.singleton is None:
-        Options.singleton = Options()
-    return Options.singleton
-    
-class Options(GObject.Object):
+class IntelligentTextCompletionOptions(object):
 
-    __gsignals__ = {
-        'options-changed' : (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, ()),
-    }
+    ## settings
+    closeBracketsAndQuotes = True
+    completeXML = True
+    detectLists = True
+    autoindentAfterFunctionOrList = True
 
+    ## buttons for settings
+    _closeBracketsAndQuotesButton = None
+    _completeXMLButton = None
+    _detectListsButton = None
+    _autoindentAfterFunctionOrListButton = None
+
+    ## configuration client
+    _GCONF_SETTINGS_DIR = "/apps/gedit-3/plugins/intelligent_text_completion"
+    _gconf_client = None
+
+    ## static singleton reference
     singleton = None
 
     def __init__(self):
-
-        GObject.Object.__init__(self)
-        self.__gconfDir = "/apps/gedit-3/plugins/intelligent_text_completion"
-
-        # default values
-        self.closeBracketsAndQuotes = True
-        self.completeXML = True
-        self.detectLists = True
-        self.autoindentAfterFunctionOrList = True
-    
         # create gconf directory if not set yet
-        client = gconf.client_get_default()        
-        if not client.dir_exists(self.__gconfDir):
-            client.add_dir(self.__gconfDir,gconf.CLIENT_PRELOAD_NONE)
-        
-        if client.dir_exists(self.__gconfDir+"/closeBracketsAndQuotes"):
-            # get the gconf keys, or stay with default if key not set
-            try:
-                self.closeBracketsAndQuotes = client.get_bool(self.__gconfDir+"/closeBracketsAndQuotes")
-                self.completeXML = client.get_bool(self.__gconfDir+"/completeXML")
-                self.detectLists = client.get_bool(self.__gconfDir+"/detectLists")
-                self.autoindentAfterFunctionOrList = client.get_bool(self.__gconfDir+"/autoindentAfterFunctionOrList")
-            except Exception, e: # catch, just in case
-                print e
-    def __del__(self):
-        # write changes to gconf
-        client = gconf.client_get_default()
-        client.set_bool(self.__gconfDir+"/closeBracketsAndQuotes", self.closeBracketsAndQuotes)
-        client.set_bool(self.__gconfDir+"/completeXML", self.completeXML)
-        client.set_bool(self.__gconfDir+"/detectLists", self.detectLists)
-        client.set_bool(self.__gconfDir+"/autoindentAfterFunctionOrList", self.autoindentAfterFunctionOrList)
+        self._gconf_client = gconf.client_get_default()
+        if not self._gconf_client.dir_exists(self._GCONF_SETTINGS_DIR):
+            self._gconf_client.add_dir(self._GCONF_SETTINGS_DIR, gconf.CLIENT_PRELOAD_NONE)
+
+        # load settings
+        self.closeBracketsAndQuotes = self._load_setting("closeBracketsAndQuotes")
+        self.completeXML = self._load_setting("completeXML")
+        self.detectLists = self._load_setting("detectLists")
+        self.autoindentAfterFunctionOrList = self._load_setting("autoindentAfterFunctionOrList")
+
+    @classmethod
+    def get_instance(cls):
+        """ Get singleton instance """
+        if cls.singleton is None:
+            cls.singleton = cls()
+        return cls.singleton
 
     def create_configure_dialog(self):
-        win = Gtk.Window()
-        win.connect("delete-event",lambda w,e: w.destroy())
-        win.set_title("Preferences")
-        win.set_position(Gtk.WIN_POS_CENTER)
-        vbox = Gtk.VBox() 
+        """ Creates configure dialog using GTK """
+        # make vertically stacking box
+        vbox = Gtk.VBox()
+        vbox.set_border_width(6)
 
-        #--------------------------------  
+        # add checkboxes
+        self._closeBracketsAndQuotesButton = self._add_setting_checkbox(
+            vbox=vbox,
+            current_value=self.closeBracketsAndQuotes,
+            helptext="Auto-close brackets and quotes",
+        )
+        self._completeXMLButton = self._add_setting_checkbox(
+            vbox=vbox,
+            current_value=self.completeXML,
+            helptext="Auto-complete XML tags",
+        )
+        self._detectListsButton = self._add_setting_checkbox(
+            vbox=vbox,
+            current_value=self.detectLists,
+            helptext="Detect lists",
+        )
+        self._autoindentAfterFunctionOrListButton = self._add_setting_checkbox(
+            vbox=vbox,
+            current_value=self.autoindentAfterFunctionOrList,
+            helptext="Auto-indent after function or list",
+        )
+        return vbox
 
-        # disable tabs
-        #notebook = gtk.Notebook()
-        #notebook.set_border_width(6)
-        #vbox.pack_start(notebook)
-
-        vbox2 = Gtk.VBox()
-        vbox2.set_border_width(6) 
-
+    def _add_setting_checkbox(self, vbox, current_value, helptext):
         box = Gtk.HBox()
-        closeBracketsAndQuotes = Gtk.CheckButton("Auto-close brackets and quotes")
-        closeBracketsAndQuotes.set_active(self.closeBracketsAndQuotes)
-        box.pack_start(closeBracketsAndQuotes,False,False,6)
-        vbox2.pack_start(box,False)
-        
-        box = Gtk.HBox()
-        completeXML = Gtk.CheckButton("Auto-complete XML tags")
-        completeXML.set_active(self.completeXML)
-        box.pack_start(completeXML,False,False,6)
-        vbox2.pack_start(box,False)
+        check_button = Gtk.CheckButton(helptext)
+        check_button.set_active(current_value)
+        box.pack_start(check_button,False,False,6)
+        check_button.connect('toggled', self._on_check_button_toggled)
+        vbox.pack_start(box, False, True, 0)
+        return check_button
 
-        box = Gtk.HBox()
-        detectLists = Gtk.CheckButton("Detect lists")
-        detectLists.set_active(self.detectLists)
-        box.pack_start(detectLists,False,False,6)
-        vbox2.pack_start(box,False)
+    def _on_check_button_toggled(self, *args):
+        # set class attributes
+        self.closeBracketsAndQuotes = self._closeBracketsAndQuotesButton.get_active()
+        self.completeXML = self._completeXMLButton.get_active()
+        self.detectLists = self._detectListsButton.get_active()
+        self.autoindentAfterFunctionOrList = self._autoindentAfterFunctionOrListButton.get_active()
 
-        box = Gtk.HBox()
-        autoindentAfterFunctionOrList = Gtk.CheckButton("Auto-indent after function or list")
-        autoindentAfterFunctionOrList.set_active(self.autoindentAfterFunctionOrList)
-        box.pack_start(autoindentAfterFunctionOrList,False,False,6)
-        vbox2.pack_start(box,False)
-        
-        # disable tabs
-        #notebook.append_page(vbox2,Gtk.Label("General"))
-        vbox.pack_start(vbox2, False)
+        # write changes to gconf
+        self._save_setting("closeBracketsAndQuotes", self.closeBracketsAndQuotes)
+        self._save_setting("completeXML", self.completeXML)
+        self._save_setting("detectLists", self.detectLists)
+        self._save_setting("autoindentAfterFunctionOrList", self.autoindentAfterFunctionOrList)
 
-        #--------------------------------       
-        vbox2 = Gtk.VBox()
-        vbox2.set_border_width(6)
+    def _save_setting(self, setting_name, value):
+        self._gconf_client.set_bool("{}/{}".format(self._GCONF_SETTINGS_DIR, setting_name), value)
 
-        button = {}
-
-        def setValues(w):
-
-            # set class attributes
-            self.closeBracketsAndQuotes = closeBracketsAndQuotes.get_active()
-            self.completeXML = completeXML.get_active()
-            self.detectLists = detectLists.get_active()
-            self.autoindentAfterFunctionOrList = autoindentAfterFunctionOrList.get_active()
-                
-            # write changes to gconf
-            client = gconf.client_get_default()
-
-            client.set_bool(self.__gconfDir+"/closeBracketsAndQuotes", self.closeBracketsAndQuotes)
-            client.set_bool(self.__gconfDir+"/completeXML", self.completeXML)
-            client.set_bool(self.__gconfDir+"/detectLists", self.detectLists)
-            client.set_bool(self.__gconfDir+"/autoindentAfterFunctionOrList", self.autoindentAfterFunctionOrList)
-
-            # commit changes and quit dialog
-            self.emit("options-changed")
-            win.destroy()
-
-        box = Gtk.HBox()
-        b = Gtk.Button(None,Gtk.STOCK_OK)
-        b.connect("clicked",setValues)
-        box.pack_end(b,False)
-        b = Gtk.Button(None,Gtk.STOCK_CANCEL)
-        b.connect("clicked",lambda w,win: win.destroy(),win)
-        box.pack_end(b,False)
-        vbox.pack_start(box,False)
-
-        win.add(vbox)
-        win.show_all()        
-        return win
-
-GObject.type_register(Options)
+    def _load_setting(self, setting_name):
+        try:
+            return self._gconf_client.get_bool("{}/{}".format(self._GCONF_SETTINGS_DIR, setting_name))
+        except Exception, e: # catch, just in case
+            print e
 
